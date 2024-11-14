3797fbf9ad312b9bc93e8715a44fe184c2bb444d
bartek <log.mazik.bartek@gmail.com>
2024-11-14 20:47:01 +0100

    openai api post setup, axios

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,46 @@
 import "./App.css";
+import axios from "axios";
+import { useState } from "react";
+
+const apiKey = process.env.REACT_APP_OPENAI_API_KEY;
+
+const fetchOpenAIResponse = async (prompt: string) => {
+  try {
+    const res = await axios.post(
+      "https://api.openai.com/v1/chat/completions",
+      {
+        model: "gpt-3.5-turbo",
+        messages: [{ role: "user", content: prompt }],
+        max_tokens: 100,
+      },
+      {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${apiKey}`,
+        },
+      }
+    );
+
+    return res.data.choices[0].message.content;
+  } catch (error) {
+    console.error("OpenAI fetch error:", error);
+  }
+};
 
 function App() {
+  const [completion, setCompletion] = useState<string | null>("TEXT");
   return (
     <>
-      <button>UPLOAD FILE</button>
+      <button
+        onClick={() => {
+          fetchOpenAIResponse("Wygeneruj losowe przysłowie").then((response) =>
+            setCompletion(response)
+          );
+        }}
+      >
+        UPLOAD FILE
+      </button>
+      <div>{completion}</div>
     </>
   );
 }
